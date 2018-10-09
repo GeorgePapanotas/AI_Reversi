859cf622130031e7fb7f82026f0a6a3c327bbddb
George Papanotas <djgiwrgostso@gmail.com>
2018-10-09 15:11:44 +0300

    node testing paused

--- src/mainTest.java
+++ src/mainTest.java
@@ -1,23 +1,41 @@
 import java.util.ArrayList;
 
 public class mainTest {
-    private node<testData> root;
-    private ArrayList<node<testData>> children;
-    public void main(String args[]){
+    private static node<testData> root;
+
+    public static void main(String args[]){
         root = new node<testData>(new testData(Integer.MIN_VALUE,Integer.MAX_VALUE,0));
-        generateChildren(root,(int) Math.random() * 5);
+        int limit =(int) (1 + Math.random() * 4);
+        //System.out.println("limit: " + limit);
+        generateChildren(root,limit);
+        traverse(root);
     }
-    private void generateChildren(node<testData> parent, int limit){
-        children = new ArrayList<>();
 
-        if(limit == 0) return;
+    private static void generateChildren(node<testData> parent, int limit){
 
-        int numOfChildren = (int) (Math.random() *5);
+        if(limit <= 0) return;
+
+        int numOfChildren = (int) ((Math.random() *4));
+        System.out.println("num of children: "+numOfChildren + "\nlimit: " + limit);
 
         for(int i = 0; i< numOfChildren; i++){
-            node<testData> child = new node<testData>(new testData(parent.getData().getAlpha(),parent.getData().getBeta(),(int) Math.random() * 5));
+            node<testData> child = new node<testData>(new testData(parent.getData().getAlpha(),parent.getData().getBeta(),(int) (Math.random() * 15)));
             parent.addChild(child);
-            generateChildren(child, --limit);
+            generateChildren(child, limit-1);
+        }
+    }
+
+    private static void traverse(node<testData> root){
+        if(root.isLeaf()){
+            System.out.println(root.getData().getScore());
+            return;
+        }else{
+            for(node<testData> child: root.children){
+                traverse(child);
+            }
+            System.out.println("-");
+            System.out.println(root.getData().getScore());
+            System.out.println("-");
         }
     }
 }
