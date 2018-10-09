5e7df8fa84c40287fc7ed398186d2af3a4134e90
JohnBelly <john_belly@hotmail.com>
2018-10-09 16:06:18 +0300

    added Moves interface

---
+++ src/Moves.java
@@ -0,0 +1,26 @@
+public interface Moves {
+    class MoveCoord{
+        private int row, col;
+
+        public MoveCoord(int row, int col){
+            this.row = row;
+            this.col = col;
+        }
+
+        public int getRow() {
+            return row;
+        }
+
+        public void setRow(int row) {
+            this.row = row;
+        }
+
+        public int getCol() {
+            return col;
+        }
+
+        public void setCol(int col) {
+            this.col = col;
+        }
+    }
+}
