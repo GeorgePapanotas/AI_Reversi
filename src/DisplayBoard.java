4f60405feba5b3ff9b8af6f8be652274417beed7
George Papanotas <djgiwrgostso@gmail.com>
2018-10-10 14:45:34 +0300

    Refinements

--- src/DisplayBoard.java
+++ src/DisplayBoard.java
@@ -2,13 +2,12 @@ import java.util.Scanner;
 
 public class DisplayBoard implements Moves{
 
-    static int xpos = -1,ypos = -1;
+
     public static void main(String [] args){
         Board b = new Board();
 
         b.display();
         System.out.println();
-        Scanner s = new Scanner(System.in);
 
         int a = 1;
         int player = 1;
@@ -35,7 +34,7 @@ public class DisplayBoard implements Moves{
         System.out.println("Player "+player+" make your move");
         int row = s.nextInt();
         int col = s.nextInt();
-        while((xpos<0 || xpos >7) || (ypos<0 || ypos>7)){
+        while((row<0 || row >7) || (col<0 || col>7)){
             System.out.println("Dude make a valid move (>=0, <=7)");
             row = s.nextInt();
             col = s.nextInt();
