ddde86641d4f678374d9659f2d1df137e7985468
JohnBelly95 <johnbelly95@gmail.com>
2018-10-10 14:37:43 +0300

    Changed ? to - on board printing

--- src/Board.java
+++ src/Board.java
@@ -5,7 +5,7 @@ public class Board {
 
         for(int i  = 0;i<8;i++){
             for(int j = 0;j<8;j++){
-                a[i][j] = '?';
+                a[i][j] = '-';
             }
         }
 
